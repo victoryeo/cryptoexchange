10c55866e295c72adab9b21e76110c6741ba1dd1
yeosiowvic <victoryeo1974@gmail.com>
2021-07-01 22:57:39 +0800

    remove unused function

--- tradeengine/order_match/main.go
+++ tradeengine/order_match/main.go
@@ -23,41 +23,6 @@ type SendResponse struct {
 	Amount  uint32 `json:"amount"`
 }
 
-func crypto_interface_get() {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://127.0.0.1:8081/", nil)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Print("http do error ")
-		fmt.Println(err.Error())
-	}
-
-	defer func() {
-		if resp != nil {
-			err = resp.Body.Close()
-			if err != nil {
-				fmt.Print("http close error ")
-				fmt.Print(err.Error())
-			}
-		}
-	}()
-	if resp != nil {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Print("http read error ")
-			fmt.Print(err.Error())
-		}
-		var responseObject Response
-		json.Unmarshal(bodyBytes, &responseObject)
-		fmt.Printf("API Response as struct %+v\n", responseObject)
-	}
-}
-
 func crypto_interface_init() {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", "http://127.0.0.1:8081/", nil)
