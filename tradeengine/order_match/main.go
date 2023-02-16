dd9bbcd34fd2a7ebc1a66328d556f7266616c5ab
yeosiowvic <victoryeo1974@gmail.com>
2023-02-16 17:08:05 +0800

    improve order processing loop with interruption handling

--- tradeengine/order_match/main.go
+++ tradeengine/order_match/main.go
@@ -8,7 +8,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/victoryeo/cryptoexchange/engine"
@@ -110,6 +114,9 @@ func main() {
 	})
 	fmt.Printf("%v\n", rdb)
 
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
 	// create the order book
 	book := engine.OrderBook{
 		BuyOrders:  make([]engine.Order, 0, ORDERBOOK_LEN),
@@ -118,63 +125,72 @@ func main() {
 	log.Printf("%v\n", book)
 
 	// create a signal channel to know when we are done
-	done := make(chan bool)
+	done := make(chan bool, 1)
 
 	// start processing orders
 	go func() {
 		for i := 1; i < ORDERBOOK_LEN; i++ {
 			var order engine.Order
-			key := "order" + strconv.Itoa(i)
-			allVal, err := rdb.HGetAll(ctx, key).Result()
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("%v\n", allVal)
-			fmt.Printf("allval %d\n", len(allVal))
-			if len(allVal) == 0 {
-				done <- true
-			} else {
-				//scanVal := rdb.HScan(ctx, "order", 0, "*", 0)
-				//fmt.Println("scan", scanVal)
-
-				for index, element := range allVal {
-					fmt.Println("Key:", index, "=>", "Value:", element)
-				}
-				/*
-					var buf bytes.Buffer
-					enc := gob.NewEncoder(&buf)
-					// encoding the map
-					err = enc.Encode(allVal)
-					fmt.Printf("%v\n", buf)
-					// convert to byte array
-					var orderHolder []byte
-					orderHolder = buf.Bytes()
-					fmt.Printf("orderHolder\n")
-					fmt.Printf("%v\n", orderHolder) */
-
-				// decode the message
-				price, err := strconv.Atoi(allVal["price"])
+			select {
+			case sig := <-sigchan:
+				fmt.Printf("Caught signal %v: terminating\n", sig)
+				done <- false
+			default:
+				key := "order" + strconv.Itoa(i)
+				allVal, err := rdb.HGetAll(ctx, key).Result()
 				if err != nil {
-					log.Println(err)
+					panic(err)
 				}
-				order.Price = uint64(price)
-				qty, err := strconv.Atoi(allVal["qty"])
-				order.Quantity = uint32(qty)
-				if allVal["type"] == "buy" {
-					order.Type = 1
+
+				if len(allVal) == 0 {
+					//done <- true
+					time.Sleep(2 * time.Second)
+					continue
 				} else {
-					order.Type = 0
+					fmt.Printf("%v\n", allVal)
+					fmt.Printf("allval %d\n", len(allVal))
+					//scanVal := rdb.HScan(ctx, "order", 0, "*", 0)
+					//fmt.Println("scan", scanVal)
+
+					for index, element := range allVal {
+						fmt.Println("Key:", index, "=>", "Value:", element)
+					}
+					/*
+						var buf bytes.Buffer
+						enc := gob.NewEncoder(&buf)
+						// encoding the map
+						err = enc.Encode(allVal)
+						fmt.Printf("%v\n", buf)
+						// convert to byte array
+						var orderHolder []byte
+						orderHolder = buf.Bytes()
+						fmt.Printf("orderHolder\n")
+						fmt.Printf("%v\n", orderHolder) */
+
+					// decode the message
+					price, err := strconv.Atoi(allVal["price"])
+					if err != nil {
+						log.Println(err)
+					}
+					order.Price = uint64(price)
+					qty, err := strconv.Atoi(allVal["qty"])
+					order.Quantity = uint32(qty)
+					if allVal["type"] == "buy" {
+						order.Type = 1
+					} else {
+						order.Type = 0
+					}
+					fmt.Printf("order\n")
+					fmt.Printf("%v\n", order)
+
+					// process the order
+					trades := book.Process(order)
+					fmt.Printf("match trade %v\n", trades)
+
+					// forware the trade to crypto API
+					crypto_interface_init()
+					crypto_interface_send(order.Quantity)
 				}
-				fmt.Printf("order\n")
-				fmt.Printf("%v\n", order)
-
-				// process the order
-				trades := book.Process(order)
-				fmt.Printf("match trade %v\n", trades)
-
-				// forware the trade to crypto API
-				crypto_interface_init()
-				crypto_interface_send(order.Quantity)
 			}
 		}
 	}()
